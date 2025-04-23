c84b093f5b3a8f3c9fbf31c456d3defd024c393d
SoyBeans01 <runescapesja@gmail.com>
2025-04-23 03:49:56 -0500

    added border animations. Consider adding a js script to allow the border animation to finish after the element is no longer hovered over

---
+++ animations.js
@@ -0,0 +1,20 @@
+//DOES NOT WORK
+
+/*
+document.addEventListener("DOMContentLoaded", () => {
+const skillItem = document.getElementsByClassName("skills-item");
+
+skillItem.addEventListener("mouseover", () => {
+    this.classList.add("spin");
+});
+
+skillItem.addEventListener("mouseout", () => {
+    setTimeout(() =>{
+        this.classList.remove("spin");
+        
+    },2000);
+    
+});
+
+});
+*/
